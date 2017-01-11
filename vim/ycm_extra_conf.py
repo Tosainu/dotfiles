ca9afcfa00366535a8eb611183150c30d95579b0
Tosainu <tosainu.maple@gmail.com>
2017-01-11 13:33:05 +0900

    compilation database

--- vim/ycm_extra_conf.py
+++ vim/ycm_extra_conf.py
@@ -42,7 +42,6 @@ flags = [
 '-DNDEBUG',
 '-x', 'c++',
 '-I', '.',
-'-I', './src',
 '-isystem', '/usr/include',
 '-isystem', '/usr/include/c++/v1',
 ]
@@ -58,7 +57,7 @@ flags = [
 #
 # Most projects will NOT need to set this to anything; you can just change the
 # 'flags' list of compilation flags. Notice that YCM itself uses that approach.
-compilation_database_folder = ''
+compilation_database_folder = 'build/'
 
 if os.path.exists( compilation_database_folder ):
   database = ycm_core.CompilationDatabase( compilation_database_folder )
