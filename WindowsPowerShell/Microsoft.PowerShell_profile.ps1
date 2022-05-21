0bc2a72b7914348e1253cdf6025725e6d27fc4ad
Kenta Sato <tosainu.maple@gmail.com>
2022-05-21 20:40:41 +0900

    powershell

---
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,34 @@
+﻿# Put this file in ~/Documents/WindowsPowerShell/ and
+# Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned
+
+# https://docs.microsoft.com/en-us/powershell/module/psreadline/set-psreadlineoption
+Set-PSReadLineOption `
+    -BellStyle None `
+    -EditMode Emacs `
+    -HistoryNoDuplicates:$true `
+    -HistorySearchCursorMovesToEnd:$true `
+    -Colors @{
+        'Command' = [ConsoleColor]::Green
+        'Comment' = [ConsoleColor]::Gray
+        'ContinuationPrompt' = [ConsoleColor]::White
+        'Default' = [ConsoleColor]::White
+        'Emphasis' = [ConsoleColor]::White
+        'Error' = [ConsoleColor]::Red
+        'InlinePrediction' = [ConsoleColor]::White
+        'Keyword' = [ConsoleColor]::White
+        'Member' = [ConsoleColor]::White
+        'Number' = [ConsoleColor]::White
+        'Operator' = [ConsoleColor]::White
+        'Parameter' = [ConsoleColor]::White
+        'String' = [ConsoleColor]::Yellow
+        'Type' = [ConsoleColor]::White
+        'Variable' = [ConsoleColor]::Green
+    }
+
+# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_prompts
+function prompt {
+    Write-Host ""
+    Write-Host "$env:UserName@$env:Computername " -ForegroundColor Yellow -NoNewLine
+    Write-Host "$(Get-Location)" -ForegroundColor Green
+    return "> "
+}
