bb31409245c0032653ffe9c05ef8fbcfa7f5a793
Kenta Sato <tosainu.maple@gmail.com>
2024-01-07 04:50:30 +0900

    Remove obsolete files

--- vscode/extensions.sh
+++
@@ -1,3 +0,0 @@
-code --install-extension ms-vscode-remote.remote-containers@0.217.4
-code --install-extension ms-vscode-remote.remote-ssh@0.74.0
-code --install-extension ms-vscode.Theme-TomorrowKit@0.1.4
